84def4081d06cde167eeabf1aa3d325cb1c885cc
xabilahu <xabilahu99@hotmail.com>
2018-09-20 11:39:16 +0200

    Añadidas clases Sistema y Web//Creados métodos en todas las clases

--- GestionWeb/src/DiccionarioPC.java
+++ GestionWeb/src/DiccionarioPC.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class DiccionarioPC {
+
+    private static DiccionarioPC miDiccionarioPC = new DiccionarioPC();
+    private ArrayList<String> listaPalabras;
+
+    private DiccionarioPC(){
+        this.listaPalabras = new ArrayList<String>();
+    }
+
+    public static DiccionarioPC getDiccionarioPC(){
+        return miDiccionarioPC;
+    }
+
+    public String buscarPalabra(String pPalabra, String pAlgo){
+        return "";
+        //TODO recordar por qué hay dos parámetros
+    }
+
+    public void insertarPalabraClave(String pPalabra){
+        this.listaPalabras.add(pPalabra);
+    }
 }
---
+++ GestionWeb/src/Web.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Web {
+
+    private String nombre;
+    private int numero;
+    private ArrayList<String> listaEnlaces;
+
+    public Web(String pNombre, int pNumero, ArrayList<String> pListaEnlaces){
+        this.nombre = pNombre;
+        this.numero = pNumero;
+        this.listaEnlaces = pListaEnlaces;
+    }
+
+    public ArrayList<String> enlacesSalientes() {
+        return this.listaEnlaces;
+    }
+
+    public Character buscarPrimerCaracter() {
+        return ' ';
+    }
+
+}
