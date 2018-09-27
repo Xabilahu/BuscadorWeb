4d360c7bcbcc486660129a6f21810c951971ecf9
yo <universidad880350@gmail.com>
2018-09-27 10:57:42 +0200

    clases sistema y web, comentados los metodos que necesitan acceso a fichero.

--- GestionWeb/src/Sistema.java
+++ GestionWeb/src/Sistema.java
@@ -1,21 +1,56 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Sistema {
 
     public void cargarDatosFichero(){
-
+        //Fichero.getFichero().cargarListaWeb(nombreFicheroWebs,nombreFicheroEnlaces);
+        //Fichero.getFichero().cargarDiccionarioPC(nombreFicheroDiccionario);
     }
 
-    public void buscarWeb(){
+    public ArrayList<String> buscarWeb(){
+        System.out.println("Inserte la web que quiera buscar");
+        Scanner sc = new Scanner(System.in);
+        String palabraClave = sc.next();
+        ArrayList<String> webs= Busqueda.getBusqueda().word2Webs(palabraClave);
+        System.out.println("Se han encontrado las siguientes webs relacionadas:");
+        sc.close();
+        return webs;
+
 
     }
 
     public void insertarWeb(){
+        System.out.println("Inserte el nombre de su web");
+        Scanner sc = new Scanner(System.in);
+        String web = sc.next();
+        //Int id=Busqueda.getBusqueda().getLengthWebs();
+        System.out.println("Inserte los enlaces salientes correspondientes a la web, en caso de no querer insertar ninguno," +
+                        "escriba -1 por pantalla:");
+        String enlace;
+        ArrayList<String> enlaces = null;
+        do{
+            enlace = sc.next();
+            if(!enlace.equals("-1")) {
+                enlaces.add(enlace);
+
+            }
+        }
+        while(!enlace.equals("-1"));
+        //Web nuevaWeb = new Web(web,,enlaces);
+        Busqueda.getBusqueda().insertarWeb(null);
+        sc.close();
 
     }
 
     public ArrayList<String> devolverEnlaces(){
-        return new ArrayList<String>();
+        System.out.println("Introduzca la web de donde desee obtener sus enlaces");
+        Scanner sc = new Scanner(System.in);
+        String web = sc.next();
+        sc.close();
+        return Busqueda.getBusqueda().enlacesSalientes(web);
+
     }
 
     public void guardarEnFichero(){
@@ -23,11 +58,19 @@ public class Sistema {
     }
 
     public ArrayList<String> listaOrdenadaWebs(){
-        return new ArrayList<String>();
+        return Busqueda.getBusqueda().webOrdenada();
     }
 
     public static void main(String[] args){
 
     }
+    private void imprimirWebs(ArrayList<String> pWebs){
+        Iterator<String> itr=pWebs.iterator();
+        String dato;
+        while(itr.hasNext()){
+            dato=itr.next();
+            System.out.println(dato);
 
+        }
+    }
 }
