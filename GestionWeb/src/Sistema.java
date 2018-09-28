3112a5abcf276733e8668fa22ab8161ff82dd823
andresperezzabala <andres.perez.zabala@gmail.com>
2018-09-28 12:30:49 +0200

    Menu creado, clase stopwatch añadida

--- GestionWeb/src/Sistema.java
+++ GestionWeb/src/Sistema.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Sistema {
 
-    public void cargarDatosFichero(){
+    public static void cargarDatosFichero(){
         Fichero.getFichero().cargarListaWeb("smallindex.txt","smallpld-arcs-1-N.txt");
         Fichero.getFichero().cargarDiccionarioPC("words.txt");
     }
 
-    public ArrayList<String> buscarWeb(){
+    public static ArrayList<String> buscarWeb(){
         System.out.println("Inserte la web que quiera buscar");
         Scanner sc = new Scanner(System.in);
         String palabraClave = sc.next();
@@ -21,7 +21,7 @@ public class Sistema {
 
     }
 
-    public void insertarWeb(){
+    public static void insertarWeb(){
         System.out.print("Inserte el nombre de su web: ");
         Scanner sc = new Scanner(System.in);
         String web = sc.next();
@@ -44,7 +44,7 @@ public class Sistema {
 
     }
 
-    public ArrayList<String> devolverEnlaces(){
+    public static ArrayList<String> devolverEnlaces(){
         System.out.print("Introduzca la web de donde desee obtener sus enlaces: ");
         Scanner sc = new Scanner(System.in);
         String web = sc.next();
@@ -53,7 +53,7 @@ public class Sistema {
 
     }
 
-    public void guardarEnFichero(){
+    public static void guardarEnFichero(){
         System.out.print("1. Guardar Webs y Enlaces\n2. Guardar palabras clave del Diccionario\n\nOpciÃ³n: ");
         Scanner sc = new Scanner(System.in);
         int opcion = sc.nextInt();
@@ -64,20 +64,40 @@ public class Sistema {
         }
     }
 
-    public ArrayList<String> listaOrdenadaWebs(){
+    public static ArrayList<String> listaOrdenadaWebs(){
         return Busqueda.getBusqueda().webOrdenada();
     }
 
     public static void main(String[] args){
+    	ArrayList<String> lista;
+    	Boolean error = false;
+    	Scanner sc = new Scanner(System.in);
+    	do {
+    		if (!error) {
+    			System.out.print("MENU:\n" +
+	    			"1. Cargar los datos desde los ficheros.\n" +
+	    			"2. Búsqueda de una página web\n" + 
+	    			"3. Inserción de una nueva página web\n" + 
+	    			"4. Devolver las páginas web enlazadas desde una web dada\n"+
+	    			"5. Guardar la lista de webs en ficheros\n" +
+	    			"6. Obtener una lista de páginas web ordenada alfabéticamente\n" +
+	    			"7. Salir\n\n Opción: ");
+    		}	 
+    		error = false;
+	    	switch (sc.nextInt()) {
+	    		case 1: cargarDatosFichero(); break;
+	    		case 2: buscarWeb(); break;
+	    		case 3: insertarWeb(); break;
+	    		case 4: devolverEnlaces(); break;
+	    		case 5: guardarEnFichero(); break;
+	    		case 6: lista = listaOrdenadaWebs(); break;
+	    		case 7: sc.close(); System.exit(0);
+	    		default: System.out.println("Error, introduce una opción correcta"); error = true;
+	    	}
+    	}while (true);
 
     }
-    private void imprimirWebs(ArrayList<String> pWebs){
-        Iterator<String> itr=pWebs.iterator();
-        String dato;
-        while(itr.hasNext()){
-            dato=itr.next();
-            System.out.println(dato);
-
-        }
+    private static void imprimirWebs(){
+    	Busqueda.getBusqueda().webOrdenada();
     }
 }
