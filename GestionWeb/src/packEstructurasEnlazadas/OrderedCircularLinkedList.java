a1e18f97b7e5bd53dd0f84f3f223c4727e69288d
xabilahu <xabilahu99@hotmail.com>
2018-10-16 11:12:09 +0200

    Added LikedStructures class support

---
+++ GestionWeb/src/packEstructurasEnlazadas/OrderedCircularLinkedList.java
@@ -0,0 +1,16 @@
+package packEstructurasEnlazadas;
+
+public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
+	
+	public void add(T elem){
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+
+
+
+
+
+
+	}
+
+
+}
