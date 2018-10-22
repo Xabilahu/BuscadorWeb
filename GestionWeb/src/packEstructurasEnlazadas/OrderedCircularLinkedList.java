011fbe73d1586117fc4e41eeabbd4096245d9050
andresperezzabala <andres.perez.zabala@gmail.com>
2018-10-22 16:10:32 +0200

    Lista ordenada

--- GestionWeb/src/packEstructurasEnlazadas/OrderedCircularLinkedList.java
+++ GestionWeb/src/packEstructurasEnlazadas/OrderedCircularLinkedList.java
@@ -1,11 +1,34 @@
 package packEstructurasEnlazadas;
 
-public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
+public class OrderedCircularLinkedList<T extends Comparable> extends CircularLinkedList<T> implements OrderedListADT<T> {
 	
-	public void add(T elem){
-
-
-
+	public void add(T elem){		
+		Node nuevo = new Node(elem);	
+		if (last != null) {
+			Node actual = last.next;
+			Node anterior = last;
+			boolean added = false;
+			
+			while (actual.next != last && !added) {
+				if (elem.compareTo(actual.data) < 0) {
+					nuevo.next = actual;
+					anterior.next = nuevo;
+					added = true;				
+				}
+				actual = actual.next;
+			}
+			if (actual.next == last && !added) {
+				actual.next = nuevo;
+				nuevo.next = actual;
+				if(elem.compareTo(actual.data) >= 0) {
+					last = nuevo;
+				}
+				added = true;
+			}
+		}else {
+			last = nuevo;
+			nuevo.next = nuevo;
+		}
+	
 	}
-
 }
