0e85010fba78f0921ef3debcaf6661d736e40e09
Xabilahu <xabilahu99@hotmail.com>
2018-11-14 12:54:11 +0100

    Graph Timing Test

--- GestionWeb/src/packPruebas/GrafoTest.java
+++ GestionWeb/src/packPruebas/GrafoTest.java
@@ -2,14 +2,22 @@ package packPruebas;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import packModelo.Fichero;
+import packModelo.Grafo;
+import packModelo.ListaWebs;
+import packTools.Stopwatch;
 
 import static org.junit.Assert.*;
 
 public class GrafoTest {
 
-    @Before
-    public void setUp() throws Exception {
+    Grafo gf = new Grafo();
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        Fichero.getFichero().cargarListaWeb("index.txt", "pld-arcs-1-N.txt");
     }
 
     @After
@@ -26,5 +34,18 @@ public class GrafoTest {
 
     @Test
     public void testEstanConectadosCamino() {
+    }
+
+    @Test
+    public void testTiempo() {
+        int numPruebas = 10;
+        gf.crearGrafo(ListaWebs.getListaWebs());
+        Stopwatch stp = new Stopwatch();
+        for (int i = 0; i < numPruebas; i++) {
+            int id1 = (int) (Math.random() * ListaWebs.getListaWebs().longitud());
+            int id2 = (int) (Math.random() * ListaWebs.getListaWebs().longitud());
+            gf.estanConectados(ListaWebs.getListaWebs().id2String(id1), ListaWebs.getListaWebs().id2String(id2));
+        }
+        System.out.println("Tiempo medio de cada prueba: " + (stp.elapsedTime() / numPruebas) + " segundos.");
     }
 }
